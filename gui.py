a5c2f1d87a35582c465c0ce62812c94d257cea29
Felix Holfelder <felixholfelder1@t-online.de>
2024-05-06 11:27:08 +0200

    added first stations

--- gui.py
+++ gui.py
@@ -1,18 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+
 import flet as ft
 
 
-def main(page: ft.Page):
-    page.update()
+def load_radio_stations():
+    f = open('radio-stations.json')
+    data = json.load(f)
+    f.close()
 
-    def changetab(e):
+    return data
+
+
+def switch_radio_station(station):
+    print(station["name"])
+
+
+def main(page: ft.Page):
+    page.theme = ft.Theme(color_scheme_seed='green')
+    page.update()
+    load_radio_stations()
+
+    def change_tab(e):
         index = e.control.selected_index
-        tab_1.visible = True if index == 0 else False
-        tab_2.visible = True if index == 1 else False
+        radio_tab.visible = True if index == 0 else False
+        settings_tab.visible = True if index == 1 else False
         page.update()
 
     page.navigation_bar = ft.NavigationBar(
-        bgcolor="blue",
-        on_change=changetab,
+        bgcolor="green",
+        on_change=change_tab,
         selected_index=0,
         destinations=[
             ft.NavigationDestination(label="Radiosender", icon=ft.icons.RADIO),
@@ -20,14 +37,40 @@ def main(page: ft.Page):
         ]
     )
 
-    tab_1 = ft.Text("Tab 1", size=30, visible=True)
-    tab_2 = ft.Text("Tab 2", size=30, visible=False)
+    radio_tab = ft.GridView(
+        expand=1,
+        runs_count=5,
+        max_extent=150,
+        child_aspect_ratio=1.0,
+        spacing=20,
+        run_spacing=50,
+        visible=True
+    )
+    settings_tab = ft.Text("Tab 2", size=30, visible=False)
+
+    page.add(radio_tab)
+
+    for i in load_radio_stations():
+        radio_tab.controls.append(
+            ft.Container(
+                on_click=switch_radio_station(i),
+                bgcolor="white",
+                border_radius=10,
+                content=ft.Image(
+                    src=f"./assets/stations/{i["logo"]}",
+                    fit=ft.ImageFit.FIT_WIDTH,
+                    repeat=ft.ImageRepeat.NO_REPEAT,
+                    border_radius=ft.border_radius.all(10),
+                )
+            )
+        )
+    page.update()
 
     page.add(
         ft.Container(
             content=ft.Column([
-                tab_1,
-                tab_2
+                radio_tab,
+                settings_tab
             ])
         )
     )
