ee470674683d340d45d5d1839e45fbc2b346dd81
Felix Holfelder <felixholfelder1@t-online.de>
2024-09-19 08:57:27 +0200

    new pics for doc

--- helper/colors.py
+++ helper/colors.py
@@ -1,5 +1,10 @@
 from PIL import ImageColor
+from colorthief import ColorThief
 
 class ColorHelper:
 	def toRgb(self, hex_value):
 		return ImageColor.getcolor(hex_value, "RGB")
+	
+	def extract_color(self, img_src):
+		ct = ColorThief(img_src)
+		palette = ct.get_palette(color_count=5)
