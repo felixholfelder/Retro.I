7f9cb53b0a0de624112d4003da6011571faba580
Felix Holfelder <felixholfelder1@t-online.de>
2024-09-24 14:57:53 +0200

    fixed toastst

--- components/GpioButton.py
+++ components/GpioButton.py
@@ -1,6 +1,7 @@
+import time
 from gpiozero import LED, Button
 
 class GpioButton:
     def __init__(self, pin, callback):
         button = Button(pin)
-        button.when_pressed = lambda e: callback()
+        button.when_released = lambda e: callback()
