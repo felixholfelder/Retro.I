1acccfa2ba18324f92eb5fe7e51c8519a3844e9d
Felix Holfelder <felixholfelder1@t-online.de>
2024-12-06 22:28:35 +0100

    added progressbars

--- components/dialogs/WifiDialog.py
+++ components/dialogs/WifiDialog.py
@@ -26,6 +26,7 @@ class WifiDialog:
                 width=500,
                 alignment=ft.alignment.center,
                 content=ft.Column(
+                    expand=True,
                     alignment=ft.MainAxisAlignment.CENTER,
                     horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     controls=[
