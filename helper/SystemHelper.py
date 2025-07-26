7d6a1c9932d032d8507f337c4444ae291b828c2a
Felix Holfelder <felixholfelder1@t-online.de>
2025-07-26 11:28:46 +0200

    refactoring: fixed pins of volume rotary

--- helper/SystemHelper.py
+++ helper/SystemHelper.py
@@ -1,24 +1,25 @@
 import os
-import sys
-import time
-import subprocess
-import keyboard
-import netifaces
 import socket
-from helper.Strip import Strip
+import subprocess
+import time
+from datetime import datetime
+
+import netifaces
+
 from helper.Audio import Audio
 from helper.Constants import Constants
 from helper.PageState import PageState
-from datetime import datetime
+from helper.Strip import Strip
 
 audio_helper = Audio()
 page_helper = PageState()
 c = Constants()
 
+
 class SystemHelper:
     strip = Strip()
     is_party = "0"
-    
+
     def __init__(self):
         self.init_party_mode()
 
@@ -33,18 +34,18 @@ class SystemHelper:
         self.strip.disable()
         time.sleep(3)
         os.system('sudo reboot')
-    
+
     def stopp_app(self, _):
         PageState.page.window_destroy()
         time.sleep(0.5)
         print("Stop App..")
         os._exit(0)
-    
+
     def get_cpu_temp(self):
         line = subprocess.run(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE).stdout.decode('utf-8')
         temp = line[5:].strip()
         return temp
-        
+
     def get_curr_date(self):
         return datetime.today().strftime('%d.%m.%Y')
 
@@ -53,30 +54,30 @@ class SystemHelper:
             return img_src
 
         return f"{c.pwd()}/assets/stations/{img_src}"
-    
+
     def get_button_img_path(self):
         return f"{c.pwd()}/assets/buttons/SB_Green.png"
-    
+
     def init_party_mode(self):
         self.is_party = os.environ.get("PARTY_MODE", "0")
-    
+
     def is_party_mode(self):
         return self.is_party == "1"
-    
+
     def open_keyboard(self):
         self.close_keyboard()
         os.system("wvkbd-mobintl -L 300")
-    
+
     def close_keyboard(self):
         os.system("pkill wvkbd-mobintl")
-    
+
     def get_default_interface(self):
         return netifaces.gateways()['default'][netifaces.AF_INET][1]
 
     def get_ip_address(self):
         ifname = self.get_default_interface()
         return netifaces.ifaddresses(ifname)[netifaces.AF_INET][0]['addr']
-    
+
     def get_hostname(self):
         return socket.gethostname()
 
@@ -100,22 +101,32 @@ class SystemHelper:
         return dns_servers
 
     def get_network_config(self):
-        config = {
-            "ip": self.get_ip_address(),
-            "hostname": self.get_hostname(),
-            "subnetmask": self.get_netmask(),
-            "mac_address": self.get_mac_address(),
-            "gateway": self.get_gateway(),
-            "dns": self.get_dns_servers()
-        }
-        return config
+        try:
+            return {
+                "ip": self.get_ip_address(),
+                "hostname": self.get_hostname(),
+                "subnetmask": self.get_netmask(),
+                "mac_address": self.get_mac_address(),
+                "gateway": self.get_gateway(),
+                "dns": self.get_dns_servers()
+            }
+        except:
+            return {
+                "ip": "",
+                "hostname": "",
+                "subnetmask": "",
+                "mac_address": "",
+                "gateway": "",
+                "dns": ""
+            }
 
     def change_screen_brightness(self, value):
         brightness = int(value / 100 * 255)
         os.system(f"sudo echo {brightness} > /sys/class/backlight/10-0045/brightness")
-    
+
     def get_curr_brightness(self):
-        line = subprocess.run(['sudo', 'cat', '/sys/class/backlight/10-0045/brightness'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        line = subprocess.run(['sudo', 'cat', '/sys/class/backlight/10-0045/brightness'],
+                              stdout=subprocess.PIPE).stdout.decode('utf-8')
         value = int(line) / 255 * 100
         if value < 10:
             return 10
