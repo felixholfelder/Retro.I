49376c05a6a8d9d168ec61ddd29110da81cecaa7
Felix Holfelder <felixholfelder1@t-online.de>
2025-09-03 20:43:48 +0200

    setup script - some optimizations

--- setup.sh
+++ setup.sh
@@ -1,153 +1,175 @@
 #!/bin/sh
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+CHECK_MARK="\xE2\x9C\x94"
+CROSS_MARK="\xE2\x9D\x8C"
+
 success() {
-  printf "\033[0;32m%s\033[0m\n" "$1"
+  printf "$GREEN $CHECK_MARK %s $GREEN" "$1"
+}
+
+error() {
+  printf "$RED $CROSS_MARK %s $RED" "$1"
+}
+
+run_step() {
+  DESCRIPTION="$1"
+  shift
+  COMMAND="$@"
+
+  echo -n "$DESCRIPTION ... "
+
+  if $COMMAND > /dev/null 2>&1; then
+    success "ERFOLGREICH"
+  else
+    error "FEHLGESCHLAGEN"
+  fi
 }
 
 # System-Einrichtung
+remove_splashscreen() {
+  fireware_config_path="/boot/firmware/config.txt"
+  disable_splash_command="disable_splash=1"
+
+  if ! grep -qxF "$disable_splash_command" "$firmware_config_path"; then
+    sudo sh -c "echo '# Disable splashscreen' >> '$fireware_config_path'"
+    sudo sh -c "echo '$disable_splash_command' >> '$fireware_config_path'"
+  fi
+}
+
+change_plymouth_pic() {
+  sudo cp -rf /home/pi/Documents/Retro.I/assets/splashscreen/splash.png /usr/share/plymouth/themes/pix/splash.png
+  sudo update-initramfs -u
+}
+
+show_plymouth_pic_on_shutdown() {
+  sudo sed -i 's/--mode=shutdown/--mode=reboot/' /usr/lib/systemd/system/plymouth-poweroff.service > /dev/null 2>&1
+}
+
+create_autostart_file() {
+  autostart_path="/etc/xdg/autostart/retroi.desktop"
+
+  if [ ! -e "$autostart_path" ]; then
+    sudo touch "$autostart_path"
+
+    sudo echo "[Desktop Entry]" > $autostart_path
+    sudo echo "Name=Retro.I" >> $autostart_path
+    sudo echo "Type=Application" >> $autostart_path
+    sudo echo "Exec=sh /home/pi/Documents/Retro.I/scripts/start.sh" >> $autostart_path
+    sudo echo "Terminal=true" >> $autostart_path
+  fi
+}
+
+hide_taskbar() {
+  wf_panel_path=/home/pi/.config/wf-panel-pi.ini
+  wf_panel_config=$(grep "^autohide=true$" "$wf_panel_path")
+
+  if [ "$wf_panel_config" != "autohide_true" ]; then
+    echo "#Hide taskbar" >> wf_panel_path
+    echo "autohide=true" >> wf_panel_path
+    echo "autohide_duration=500" >> wf_panel_path
+  fi
+}
+
+remove_background_image() {
+  # TODO - Hintergrund entfernen
+  echo ""
+}
+
+install_easyeffects() {
+  sudo apt-get install easyeffects -y -qqq
+
+  # Fehlenden config order erstellen
+  mkdir -p /home/pi/.config/easyeffects/output
+
+  # TODO - easyeffects config nach /home/pi/.config/easyeffects/output/retroi.json kopieren
+  sudo cp /home/pi/Documents/Retro.I/assets/effects/effects.json /home/pi/.config/easyeffects/output/retroi.json
+}
+
+setup_venv() {
+  python -m venv /home/pi/Documents/Retro.I/.venv
+
+  # ~.bashrc anpassen, dass bei jedem Terminalstart .venv gestartet wird
+  bashrc_path=/home/pi/.bashrc
+  bashrc_config=$(grep "^.venv/bin/activate" "$wf_panel_path")
+
+  if [ "$bashrc_config" != "autohide_true" ]; then
+    echo "#Activate venv" >> bashrc_path
+    echo "cd /home/pi/Documents/Retro.I" >> bashrc_path
+    echo "source .venv/bin/activate" >> bashrc_path
+  fi
+}
+
+setup_alsaaudio() {
+  sudo apt-get install libasound2-dev -y -qqq
+}
+
+setup_fletui() {
+  sudo apt install libmpv-dev mpv -y -qqq
+
+  # Symlink erstellen - für flet-ui
+  sudo ln -s -f /usr/lib/aarch64-linux-gnu/libmpv.so /usr/lib/aarch64-linux-gnu/libmpv.so.1
+}
+
+install_python_packages() {
+  pip install -r requirements.txt -q
+}
 
 # TODO - cooles ASCII-Art zu Beginn :D
 
 # Splashscreen entfernen
-printf "Entferne Splashscreen... "
-
-fireware_config_path="/boot/firmware/config.txt"
-disable_splash_command="disable_splash=1"
-
-firware_config=$(grep "^$disable_splash_command$" "$firmware_config_path")
-
-if [ "$firmware_config" != "$disable_splash_command" ]; then
-  sudo sh -c "echo '#Disable splashscreen' >> fireware_config_path"
-  sudo sh -c "echo '$disable_splash' >> fireware_config_path"
-fi
-
-success "ERFOLGREICH"
+run_step "Entferne Splashscreen" remove_splashscreen
 
 # Plymouth: Bild beim Systemstart ändern (in eigene Datei)
-printf "Plymouth Bild bei Systemstart ändern... "
-
-sudo cp -rf /home/pi/Documents/Retro.I/assets/splashscreen/splash.png /usr/share/plymouth/themes/pix/splash.png
-sudo update-initramfs -u
-
-success "ERFOLGREICH"
+run_step "Plymouth Bild bei Systemstart ändern" change_plymouth_pic
 
 # Plymouth - Bild beim Shutdown anzeigen
-printf "Plymouth Bild bei Shutdown zeigen... "
-
-sed 's/ExecStart=\/usr\/sbin\/plymouthd --mode=shutdown --attach-to-session/ExecStart=\/usr\/sbin\/plymouthd --mode=reboot --attach-to-session/' '/usr/lib/systemd/system/plymouth-poweroff.service'
-
-success "ERFOLGREICH"
+run_step "Plymouth Bild bei Shutdown zeigen" show_plymouth_pic_on_shutdown
 
 # Autostart-Datei erstellen
-printf "Erstelle Autostart-Datei... "
-autostart_path="/etc/xdg/autostart/retroi.desktop"
-
-if [ ! -e "$autostart_path" ]; then
-  sudo touch "$autostart_path"
-
-  sudo echo "[Desktop Entry]" > $autostart_path
-  sudo echo "Name=Retro.I" >> $autostart_path
-  sudo echo "Type=Application" >> $autostart_path
-  sudo echo "Exec=sh /home/pi/Documents/Retro.I/scripts/start.sh" >> $autostart_path
-  sudo echo "Terminal=true" >> $autostart_path
-fi
-
-success "ERFOLGREICH"
+run_step "Erstelle Autostart-Datei" create_autostart_file
 
 # Taskbar ausblenden
-printf "Taskbar ausblenden... "
+run_step "Taskbar ausblenden" hide_taskbar
 
-wf_panel_path=/home/pi/.config/wf-panel-pi.ini
-wf_panel_config=$(grep "^autohide=true$" "$wf_panel_path")
-
-if [ "$wf_panel_config" != "autohide_true" ]; then
-  echo "#Hide taskbar" >> wf_panel_path
-  echo "autohide=true" >> wf_panel_path
-  echo "autohide_duration=500" >> wf_panel_path
-fi
-
-success "ERFOLGREICH"
-
-# TODO - Hintergrund entfernen
+run_step "Hintergrund entfernen" remove_background_image
 
 # SSH aktivieren
-printf "Aktiviere SSH... "
-sudo raspi-config nonint do_ssh 0
-success "ERFOLGREICH"
+run_step "Aktiviere SSH" sudo raspi-config nonint do_ssh 0
 
 # VNC aktivieren
-printf "Aktiviere VNC... "
-sudo raspi-config nonint do_vnc 0
-success "ERFOLGREICH"
+run_step "Aktiviere VNC" sudo raspi-config nonint do_vnc 0
 
 # SPI aktivieren
-printf "Aktiviere SPI... "
-sudo raspi-config nonint do_spi 0
-success "ERFOLGREICH"
+run_step "Aktiviere SPI" sudo raspi-config nonint do_spi 0
 
 # easyeffects installieren
-printf "Installiere easyeffects... "
-sudo apt-get install easyeffects -y -qqq
-
-# Fehlenden config order erstellen
-mkdir -p /home/pi/.config/easyeffects/output
-
-# TODO - easyeffects config nach /home/pi/.config/easyeffects/output/retroi.json kopieren
-sudo cp /home/pi/Documents/Retro.I/assets/effects/effects.json /home/pi/.config/easyeffects/output/retroi.json
-
-success "ERFOLGREICH"
+run_step "Installiere easyeffects" install_easyeffects
 
 # Bildschirm-Tastatur installieren
-printf "Installiere Bildschirmtastatur... "
-sudo apt-get install wvkbd -qq
-success "ERFOLGREICH"
-
-success "Systemeinrichtung abgeschlossen!"
-printf "\nWeiter mit App-Einrichtung...\n"
+run_step "Installiere Bildschirmtastatur" sudo apt-get install wvkbd -qq
 
 # System-Einrichtung abgeschlossen!
+success "Systemeinrichtung abgeschlossen!"
 
 # Weiter mit App-Einrichtung:
+printf "\nWeiter mit App-Einrichtung...\n"
 
 # .venv einrichten (python -m venv /home/pi/Documents/Retro.I/.venv)
-printf "VENV einrichten... "
-python -m venv /home/pi/Documents/Retro.I/.venv
-
-# ~.bashrc anpassen, dass bei jedem Terminalstart .venv gestartet wird
-bashrc_path=/home/pi/.bashrc
-bashrc_config=$(grep "^.venv/bin/activate" "$wf_panel_path")
-
-if [ "$bashrc_config" != "autohide_true" ]; then
-  echo "#Activate venv" >> bashrc_path
-  echo "cd /home/pi/Documents/Retro.I" >> bashrc_path
-  echo "source .venv/bin/activate" >> bashrc_path
-fi
-
-success "ERFOLGREICH"
+run_step "VENV einrichten" setup_venv
 
 # Pakete für alsaaudio installieren
-printf "Installiere Pakete für alsaaudio... "
-sudo apt-get install libasound2-dev -y -qqq
-success "ERFOLGREICH"
+run_step "Installiere Pakete für alsaaudio" setup_alsaaudio
 
 # Pakete für flet-ui installieren
-printf "Installiere Pakete für flet-ui... "
-
-sudo apt install libmpv-dev mpv -y -qqq
-
-# Symlink erstellen - für flet-ui
-sudo ln -s -f /usr/lib/aarch64-linux-gnu/libmpv.so /usr/lib/aarch64-linux-gnu/libmpv.so.1
-
-success "ERFOLGREICH"
+run_step "Installiere Pakete für flet-ui" setup_fletui
 
 # TODO - User fragen, wie viele LED's sein LED-Streifen hat
 # -> Mit Hinweis "Wichtig für Animation der Lautstärke"
 
 # pip install -r requirements.txt
-printf "Installiere Python-Pakete... "
-pip install -r requirements.txt -q
-
-success "ERFOLGREICH"
+run_step "Installiere Python-Pakete" install_python_packages
 
 success "Setup erfolgreich abgeschlossen!\n"
 
